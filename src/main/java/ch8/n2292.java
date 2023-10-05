d6705b46b10c55378095206fbb1d7894f81f25d7
limbaba1120 <kwlim1120ss@gmail.com>
2023-10-05 22:46:30 +0900

    n2292 solved

--- src/main/java/ch8/n2292.java
+++ src/main/java/ch8/n2292.java
@@ -6,6 +6,20 @@ public class n2292 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println();
+        int N = scan.nextInt();
+
+        int count = 1;
+        int limit = 2;
+
+        if (N == 1) {
+            System.out.println(1);
+        } else {
+            while (limit <= N) {
+                limit = limit + (count * 6);
+                count++;
+            }
+            System.out.println(count);
+        }
     }
 }
+
