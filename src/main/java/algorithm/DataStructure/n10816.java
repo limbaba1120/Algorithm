eb41cb128f651de085347b6cf322eb14c3de4fec
limbaba1120 <limkw1120ss@gmail.com>
2024-01-27 11:27:01 +0900

    algorithm 24.01.27

---
+++ src/main/java/algorithm/DataStructure/n10816.java
@@ -0,0 +1,75 @@
+package algorithm.DataStructure;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class n10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int A[] = new int[N];
+        int count = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < M; i++) {
+            int key = Integer.parseInt(st.nextToken());
+
+            sb.append(upperBound(A, key) - lowerBound(A, key)).append('\n');
+        }
+        System.out.println(sb);
+
+
+    }
+
+    private static int lowerBound(int[] A, int key) {
+        int lo = 0;
+        int hi = A.length;
+
+        // lo 가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (key <= A[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+    private static int upperBound(int[] A, int key) {
+        int lo = 0;
+        int hi = A.length;
+
+        // lo 가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (key < A[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+}
