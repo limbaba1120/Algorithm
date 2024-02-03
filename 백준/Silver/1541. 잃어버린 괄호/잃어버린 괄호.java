3c2fd8efa89bb84fad29731fe654dd1af58d22bd
KeonWooLim <102224840+limbaba1120@users.noreply.github.com>
2024-02-03 21:57:13 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Main {
+    static int answer = 0;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String example = scan.nextLine();
+        String[] str = example.split("-");
+
+        for (int i = 0; i < str.length; i++) {
+            int temp = mySum(str[i]);
+            if (i == 0) {
+                answer += temp;
+            } else {
+                answer -= temp;
+            }
+        }
+        System.out.println(answer);
+
+    }
+
+    private static int mySum(String s) {
+        int sum = 0;
+        String[] str = s.split("[+]");
+        for (int i = 0; i < str.length; i++) {
+            sum += Integer.parseInt(str[i]);
+        }
+        return sum;
+    }
+}
