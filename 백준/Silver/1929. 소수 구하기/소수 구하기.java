15753bdd67d6633bea1e802931bf09e372838d88
KeonWooLim <102224840+limbaba1120@users.noreply.github.com>
2024-02-04 21:56:49 +0900

    [Silver III] Title: 소수 구하기, Time: 928 ms, Memory: 37772 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int M = scan.nextInt();
+        int N = scan.nextInt();
+
+        int[] A = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            A[i] = i;
+        }
+
+        int rootN = (int) Math.sqrt(N);
+
+        for (int i = 1; i <= rootN; i++) {
+            if (i == 1) {
+                A[i] = 0;
+            }
+            if (A[i] == 0) continue;
+
+            for (int j = i + i; j <= N; j = j + i) {
+                A[j] = 0;
+            }
+        }
+
+        for (int i = M; i <= N; i++) {
+            if (A[i] != 0) {
+                System.out.println(A[i]);
+            }
+        }
+    }
+}
