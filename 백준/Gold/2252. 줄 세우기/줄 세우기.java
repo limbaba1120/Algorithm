e9dce6d85fd3e7960231c2812487cba22aae6b42
KeonWooLim <102224840+limbaba1120@users.noreply.github.com>
2024-02-06 18:43:13 +0900

    [Gold III] Title: 줄 세우기, Time: 1476 ms, Memory: 179820 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,26 +1,28 @@
+
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-//     static ArrayList<Integer>[] A;
-    static ArrayList<ArrayList<Integer>> A;
+     static ArrayList<Integer>[] A;
+//    static ArrayList<ArrayList<Integer>> A;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
         int M = scan.nextInt();
+        A = new ArrayList[N + 1];
 
-        A = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            A.add(new ArrayList<>());
+        for (int i = 1; i <= N; i++) {
+            A[i] = new ArrayList<>();
         }
         int[] indegree = new int[N + 1];
 
         for (int i = 0; i < M; i++) {
             int S = scan.nextInt();
             int E = scan.nextInt();
-            A.get(S).add(E);
+            A[S].add(E);
             indegree[E]++; //진입차수 배열 데이터 저장 부분
         }
         //위상정렬 실행
@@ -33,7 +35,8 @@ public class Main {
         while (!queue.isEmpty()) {
             int now = queue.poll();
             System.out.print(now + " ");
-            for (int next : A.get(now)) {
+
+            for (int next : A[now]) {
                 indegree[next]--;
                 if (indegree[next] == 0) {
                     queue.offer(next);
