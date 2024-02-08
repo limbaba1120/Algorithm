0ac73f498c32565826c7b62384a9549ecf7a3b9d
limbaba1120 <kwlim1120ss@gmail.com>
2024-02-08 21:23:01 +0900

    algorithm 24.2.8

--- src/main/java/algorithm/DataStructure/n1707.java
+++ src/main/java/algorithm/DataStructure/n1707.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class n1707 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
     }
 }
