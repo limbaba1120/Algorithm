a58c41d261f828ab016c4f1ba4026c13782f12ce
limbaba1120 <limkw1120ss@gmail.com>
2024-02-08 21:19:23 +0900

    algorithm 24.2.8

---
+++ src/main/java/algorithm/DataStructure/n1707.java
@@ -0,0 +1,9 @@
+package algorithm.DataStructure;
+
+import java.util.Scanner;
+
+public class n1707 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
