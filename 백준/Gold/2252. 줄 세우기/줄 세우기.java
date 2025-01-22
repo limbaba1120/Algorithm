b46213f879e5c13a1de97c223d99aac3be825380
KeonWooLim <kwlim1120ss@gmail.com>
2025-01-22 12:11:42 +0900

    [Gold III] Title: 줄 세우기, Time: 1304 ms, Memory: 183924 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,47 +1,63 @@
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
-
 public class Main {
-     static ArrayList<Integer>[] A;
-//    static ArrayList<ArrayList<Integer>> A;
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int M = scan.nextInt();
-        A = new ArrayList[N + 1];
 
-        for (int i = 1; i <= N; i++) {
-            A[i] = new ArrayList<>();
-        }
-        int[] indegree = new int[N + 1];
+	static int n, m;
+	static List<Integer>[] list;
+	static boolean[] isVisited;
+	static int[] inDegree;
 
-        for (int i = 0; i < M; i++) {
-            int S = scan.nextInt();
-            int E = scan.nextInt();
-            A[S].add(E);
-            indegree[E]++; //진입차수 배열 데이터 저장 부분
-        }
-        //위상정렬 실행
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 1; i <= N; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
-            }
-        }
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
-            System.out.print(now + " ");
 
-            for (int next : A[now]) {
-                indegree[next]--;
-                if (indegree[next] == 0) {
-                    queue.offer(next);
-                }
-            }
-        }
-    }
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		m = scan.nextInt();
+
+		list = new ArrayList[n + 1];
+		isVisited = new boolean[n + 1];
+		inDegree = new int[n + 1];
+
+		for (int i = 0; i <= n; i++) {
+			list[i] = new ArrayList<>();
+		}
+
+		for (int i = 0; i < m; i++) {
+			int start = scan.nextInt();
+			int end = scan.nextInt();
+
+			list[start].add(end);
+			inDegree[end]++;
+		}
+
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 1; i <= n; i++) {
+			if (inDegree[i] == 0) {
+				queue.add(i);
+			}
+		}
+
+		while (!queue.isEmpty()) {
+			int now = queue.poll();
+			isVisited[now] = true;
+
+			System.out.print(now + " ");
+			for (int next : list[now]) {
+				if (isVisited[next]) {
+					continue;
+				}
+				inDegree[next]--;
+				if (inDegree[next] == 0) {
+					queue.add(next);
+				}
+			}
+		}
+
+	}
+
 }
